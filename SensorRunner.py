0816be00e959f1934daa55efa00ddfc05cc97d98
charlescates <149528073+charlescates@users.noreply.github.com>
2023-11-29 14:39:14 -0500

    Remove SensorRunner.py and add thing.py

--- SensorRunner.py
+++
@@ -1,4 +0,0 @@
-from plant_monitor import PlantMonitor
-
-myMonitor = PlantMonitor()
-print(myMonitor.get_temp())
--- thing.py
+++ thing.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from plant_monitor import PlantMonitor
+myMonitor = PlantMonitor()
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Temperature: ' + myMonitor.get_temp() + '\nHumidity: ' + myMonitor.get_humidity()
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
