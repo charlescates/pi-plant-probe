5c71cea92e132c2ffaaba9b1744b2f4c058aaa63
charlescates <149528073+charlescates@users.noreply.github.com>
2023-11-29 14:44:33 -0500

    Update thing.py

--- thing.py
+++ thing.py
@@ -4,6 +4,6 @@ myMonitor = PlantMonitor()
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Temperature: ' + myMonitor.get_temp() + '\nHumidity: ' + myMonitor.get_humidity()
+    return 'Temperature: ' + str(myMonitor.get_temp()) + '\nHumidity: ' + str(myMonitor.get_humidity())
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
